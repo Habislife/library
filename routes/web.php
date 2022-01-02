01e70f718dcc473a7bd6072fe09e8b6f7cb3cf95
Habis <devkotahancy@gmail.com>
2022-01-02 20:55:47 +0545

    book reservation test

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('books', 'App\Http\Controllers\BooksController@store');
+Route::patch('books/{book}', 'App\Http\Controllers\BooksController@update');
